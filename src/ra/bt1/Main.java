eb01993074fbbac1be4330764217cbf1d08b5c01
tanquynh <tanquynh1605@gmail.com>
2023-10-26 07:59:36 +0700

    Initial commit

---
+++ src/ra/bt1/Main.java
@@ -0,0 +1,15 @@
+package ra.bt1;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberGenerator generator1 = new NumberGenerator(Thread.MAX_PRIORITY);
+        NumberGenerator generator2 = new NumberGenerator(Thread.MIN_PRIORITY);
+
+        Thread thread1 = new Thread(generator1);
+        Thread thread2 = new Thread(generator2);
+
+        thread1.start();
+        thread2.start();
+
+    }
+}
